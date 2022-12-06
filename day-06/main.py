25883c7755df23c6b9ae704fd45d9969493b7004
Grant Ramsay <seapagan@gmail.com>
2022-12-06 10:01:51 +0000

    add day 6
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ day-06/main.py
@@ -0,0 +1,23 @@
+"""Advent of Code 2022, 5th Dec."""
+
+
+def load_data() -> str:
+    with open("input-data.txt") as f:
+        data = f.read()
+        return data
+
+
+def find_answer(signal: str, width):
+    """Find the packet marker position for the input signal."""
+    for index in range(len(signal)):
+        sample = signal[index : index + width]
+        if len(sample) == len(set(sample)):
+            return index + width
+
+
+signal = load_data()
+
+packet_marker_index = find_answer(signal, 4)
+message_index = find_answer(signal, 14)
+print(f"Packet Index is {packet_marker_index}")
+print(f"Message Index is {message_index}")
